f2f5b9c46b6554c4f7bda20d474a21892ea918c7
lastro1206 <irene051206@naver.com>
2025-05-10 13:38:07 +0900

    first commit

---
+++ src/assignments/example.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Todo() {
+  // 과제1-1: 7-1, 7-2강을 듣고 이곳에 투두리스트 컴포넌트를 작성해주세요.
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  return (
+    <div>
+      <h1>투두리스트 ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='오늘의 투두'
+        />
+        <button>추가</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
